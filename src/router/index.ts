b95413f391039fbd2712d49d5d17fb288d11f22b
UIE47061 <ue4761@gmail.com>
2025-12-11 14:11:23 +0800

    [modify] 修正 Vite 配置中的基礎路徑

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHistory('/Tetris-frontend/'),
+    history: createWebHistory('/Tetris/'),
     routes
 })
 
